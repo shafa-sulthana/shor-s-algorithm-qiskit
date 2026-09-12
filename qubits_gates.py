8d6800c92b48d5981c9105d31942e23203f900ab
shafa-sulthana <r220740@rguktrkv.ac.in>
2026-09-12 10:44:49 +0000

    Update qubits_gates.py

--- qubits_gates.py
+++ qubits_gates.py
@@ -3,7 +3,16 @@ from qiskit import QuantumCircuit
 # Create a quantum circuit with one qubit
 qc = QuantumCircuit(1)
 
-# Apply Hadamard gate
+# X gate 
+qc.x(0)
+
+# Y gate
+qc.y(0)
+
+# Z gate
+qc.z(0)
+
+# H gate - creates superposition
 qc.h(0)
 
 # Display the circuit
